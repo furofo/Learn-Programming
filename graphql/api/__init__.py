13236fd4fbc149fd19de6ee5404fb246c6b2c93c
Christian Malpass <chriscellphonerepair@gmail.com>
2025-01-28 22:29:03 +0200

    postgres version setup

--- graphql/api/__init__.py
+++ graphql/api/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://myuser:mypassword@localhost/mydatabase"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
 @app.route('/')
 def hello():
