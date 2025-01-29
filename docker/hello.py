f075316c124e03f8c08cd93891fd956a7a5be921
Christian Malpass <chriscellphonerepair@gmail.com>
2025-01-29 20:08:43 +0200

    using debugging with docker

---
+++ docker/hello.py
@@ -0,0 +1,11 @@
+# youtube https://www.youtube.com/watch?v=cJbvcH0JNGA
+### docker practice
+# downloaded docker image from docker and built on top of it
+# then docker build -t hello .docker build -t hello . to build the image
+# docker run hello to run the image
+# remember have to rebuild it each time I change this way
+# used dev containers
+a=1
+b=2
+c=a+b
+print("hello steve")
